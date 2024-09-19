0cfeed85961f70a6d4b4ec2d8318e7ba816f5bf3
Ivan Belov <eloueone@mail.ru>
2024-09-19 21:18:35 +0300

    update 20.09

--- TgBot.py
+++ TgBot.py
@@ -5,6 +5,7 @@ import sys
 import os
 from dotenv import load_dotenv
 from functions import DB
+from Buttons import keyboard, keyboard_info
 
 from aiogram import Bot, Dispatcher, html, F
 from aiogram.client.default import DefaultBotProperties
@@ -33,47 +34,6 @@ class Top(StatesGroup):
     top = State()
     film_search = State()
     authority = State()
-
-
-button_1 = InlineKeyboardButton(
-    text='Показать фильм',
-    callback_data='button_1_pressed'
-)
-button_2 = InlineKeyboardButton(
-    text='Искать дальше',
-    callback_data='button_2_pressed'
-)
-keyboard = InlineKeyboardMarkup(
-    inline_keyboard=[[button_1],
-                     [button_2]]
-)
-button_info_1 = InlineKeyboardButton(
-    text='О проекте',
-    callback_data='button_info_1_pressed'
-)
-button_info_2 = InlineKeyboardButton(
-    text='Полный список фильмов',
-    callback_data='button_info_2_pressed'
-)
-button_info_3 = InlineKeyboardButton(
-    text='Авторы постеров',
-    callback_data='button_info_3_pressed'
-)
-button_info_4 = InlineKeyboardButton(
-    text='Обратная связь',
-    callback_data='button_info_4_pressed'
-)
-button_info_5 = InlineKeyboardButton(
-    text='Поддержать бота❤',
-    callback_data='button_info_5_pressed'
-)
-keyboard_info = InlineKeyboardMarkup(
-    inline_keyboard=[[button_info_1],
-                     [button_info_2],
-                     [button_info_3],
-                     [button_info_4],
-                     [button_info_5]]
-)
 
 
 @dp.message(CommandStart())
